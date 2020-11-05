5759d992a0478785591f4bda643d4ddc93c9490f
Sam Lalli <2sam.lalli@gmail.com>
2020-11-05 16:08:04 -0700

    2 of 3 endpoints wired

--- src/main/java/com/lambdaschool/javazoos/services/ZooService.java
+++ src/main/java/com/lambdaschool/javazoos/services/ZooService.java
@@ -1,4 +1,12 @@
 package com.lambdaschool.javazoos.services;
 
+import com.lambdaschool.javazoos.models.Zoo;
+
+import java.util.List;
+
 public interface ZooService {
+
+    List<Zoo> findAll();
+
+    Zoo findZooById(long id);
 }
